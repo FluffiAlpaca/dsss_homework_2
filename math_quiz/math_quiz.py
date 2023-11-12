b9e727203de71caef8cc79a9da8904a6383aad39
FluffiAlpaca <jinayoo3138@gmail.com>
2023-11-12 11:55:46 +0100

    Update math_quiz.py

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,43 +1,60 @@
 import random
 
 
-def function_A(min, max):
+def rand_int_gen(min, max):
     """
-    Random integer.
+    Generate random integer with specified lower & upper bounds
     """
-    return random.randint(min, max)
+    try:
+         rand = random.randint(min,max)
+    except:
+        rand = random.randint(int(min), int(max))
+    return rand
 
 
-def function_B():
-    return random.choice(['+', '-', '*'])
+def rand_oper_gen():
+    """
+        Generate random arithmetic operator
+    """
+    return random.choice(['+', '-', '*', '/'])
 
 
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
+def rand_answer(n1, n2, oper):
+    """
+            Generate random problem and answer
+    """
+    p = f"{n1} {oper} {n2}"
+    if oper == '+': a = n1 + n2
+    elif oper == '-': a = n1 - n2
+    elif oper == '*':a = n1 * n2
+    else: a = n1/n2
+
     return p, a
 
 def math_quiz():
+    """
+        Generate quiz questions and count user's score
+    """
     s = 0
-    t_q = 3.14159265359
+    t_q = 3
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
     for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+        n1 = rand_int_gen(1, 10)
+        n2 = rand_int_gen(1, 5.5)
+        o = rand_oper_gen()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
+        PROBLEM, ANSWER = rand_answer(n1, n2, o)
         print(f"\nQuestion: {PROBLEM}")
         useranswer = input("Your answer: ")
         useranswer = int(useranswer)
 
-        if useranswer == ANSWER:
+        if useranswer == ANSWER: #correct answer
             print("Correct! You earned a point.")
             s += -(-1)
-        else:
+        else: #incorrect answer
             print(f"Wrong answer. The correct answer is {ANSWER}.")
 
     print(f"\nGame over! Your score is: {s}/{t_q}")
