69fd80bd5f5ada77d03df7800bf16f5726ccb1d1
FluffiAlpaca <jinayoo3138@gmail.com>
2023-11-12 13:50:17 +0100

    modify setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='Jina Yoo',
+      version='1.0',
+      description='DSSS Homework 2',
+      author='Jina Yoo',
+      author_email='jina.yoo@fau.de',
+      url='https://github.com/FluffiAlpaca/dsss_homework_2/',
+      packages=['distutils', 'distutils.command'])
